062cef7f00ca7bb24075ad265ef9ffb59713ae03
C Jones <cljone23@gmail.com>
2021-02-21 11:23:26 -0500

    adding WeatherData class, pseduo code for main.py

--- main.py
+++ main.py
@@ -1,2 +1,7 @@
-import json
-import requests
+# collect details
+#  dog's name
+#  zip code where your dog likes to walk
+#  availability
+#  duration
+# get weather data
+# analyze and return recommendations
